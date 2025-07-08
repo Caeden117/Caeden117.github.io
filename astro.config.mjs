1f732047b93b903428ea37d39a66f92f6dfc39ef
Caeden117 <caeden.s@outlook.com>
2025-07-08 15:26:43 -0700

    back into the grindset

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()]
 });
