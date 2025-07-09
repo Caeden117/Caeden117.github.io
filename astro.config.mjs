9a1f765de754b0a866233c4570cdc4b908437015
Caeden117 <caeden.s@outlook.com>
2025-07-08 23:44:15 -0700

    Update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Caeden117.github.io',
+  site: 'https://caeden.dev',
 
   vite: {
       plugins: [tailwindcss()],
